a28b4a36281069bd680984d3f3a0d7f072107a3c
Shubham Malik <1197smalik@gmail.com>
2026-01-21 01:07:14 +0530

    Updated settings due to CORS issue

--- tasktracker/settings.py
+++ tasktracker/settings.py
@@ -128,6 +128,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://tasktracker.shubhammalik.com",
+    "api-tasktracker.shubhammalik.com",
 ]
 
 # REST Framework settings
